2277dcb78651cfe056455f481581f13f7d437b52
AndrewMosh <99655206+AndrewMosh@users.noreply.github.com>
2022-05-31 22:05:48 +0300

    Add files via upload

---
+++ contacts.js
@@ -0,0 +1,10 @@
+const contact = document.getElementById("contacts");
+const contbar = document.getElementById("conbar");
+const contbar2 = document.getElementById("conbar2");
+const contbar3 = document.getElementById("conbar3");
+
+contact.onclick = function cont() {
+  contbar.style.color = "#DEB887";
+  contbar2.style.color = "#DEB887";
+  contbar3.style.color = "#DEB887";
+};
