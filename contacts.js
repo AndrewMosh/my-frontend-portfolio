79a4f9add8c957793ac5e48b7629d6908cbe6920
AndrewMosh <99655206+AndrewMosh@users.noreply.github.com>
2022-06-01 14:43:33 +0300

    Add files via upload

--- contacts.js
+++ contacts.js
@@ -1,10 +1,5 @@
-const contact = document.getElementById("contacts");
-const contbar = document.getElementById("conbar");
-const contbar2 = document.getElementById("conbar2");
-const contbar3 = document.getElementById("conbar3");
-
-contact.onclick = function cont() {
-  contbar.style.color = "#DEB887";
-  contbar2.style.color = "#DEB887";
-  contbar3.style.color = "#DEB887";
-};
+$(function () {
+  $("#contacts").click(function () {
+    $(".icon-bar a").fadeOut().fadeIn();
+  });
+});
